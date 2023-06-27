7b29f45628bd9c4ab994773a857ca4849b893d76
tomfacal <88186642+tomfacal@users.noreply.github.com>
2023-06-27 15:01:04 +0200

    añadidas funciones y test

--- src/app/karma-jasmine.service.spec.ts
+++ src/app/karma-jasmine.service.spec.ts
@@ -1,13 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { KarmaJasmineService } from './karma-jasmine.service';
+let service: KarmaJasmineService;
+
+beforeEach(() => {
+  TestBed.configureTestingModule({});
+  service = TestBed.get(KarmaJasmineService);
+});
 
 describe('KarmaJasmineService', () => {
-  let service: KarmaJasmineService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.get(KarmaJasmineService);
-  });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
@@ -29,3 +30,59 @@ describe('KarmaJasmineService', () => {
   });
 
 });
+describe('Función de resta', () => {
+
+  it('debería restar dos números correctamente', () => {
+    const resultado = service.restar(5, 3);
+    expect(resultado).toBe(2);
+  });
+
+  it('debería restar números negativos correctamente', () => {
+    const resultado = service.restar(-5, -3);
+    expect(resultado).toBe(-2);
+  });
+
+  it('debería restar un número positivo y un número negativo correctamente', () => {
+    const resultado = service.restar(10, -5);
+    expect(resultado).toBe(15);
+  });
+});
+describe('Función de multiplicación', () => {
+  it('debería multiplicar dos números correctamente', () => {
+    const resultado = service.multiplicar(2, 3);
+    expect(resultado).toBe(6);
+  });
+
+  it('debería multiplicar un número positivo y un número negativo correctamente', () => {
+    const resultado = service.multiplicar(5, -3);
+    expect(resultado).toBe(-15);
+  });
+
+  it('debería multiplicar por cero correctamente', () => {
+    const resultado = service.multiplicar(10, 0);
+    expect(resultado).toBe(0);
+  });
+});
+describe('Función de división', () => {
+  it('debería dividir dos números correctamente', () => {
+    const resultado = service.dividir(6, 3);
+    expect(resultado).toBe(2);
+  });
+
+  it('debería dividir un número positivo y un número negativo correctamente', () => {
+    const resultado = service.dividir(15, -3);
+    expect(resultado).toBe(-5);
+  });
+  it('debería dividir dos negativos correctamente', () => {
+    const resultado = service.dividir(-15, -3);
+    expect(resultado).toBe(5);
+  });
+  it('debería lanzar un error al intentar dividir entre cero', () => {
+    expect(() => service.dividir(10, 0)).toThrowError('No se puede dividir entre cero');
+  });
+});
+
+
+
+
+
--- src/app/karma-jasmine.service.ts
+++ src/app/karma-jasmine.service.ts
@@ -10,6 +10,17 @@ export class KarmaJasmineService {
   sumar(a: number, b: number): number {
     return a + b;
   }
+  restar(a: number, b: number): number {
+    return a - b;
+  }
+  multiplicar(a: number, b: number): number {
+    return a * b;
+  }
+  dividir(a: number, b: number): number {
+    if (b === 0) {
+      throw new Error('No se puede dividir entre cero');
+    }
 
-
+    return a / b;
+  }
 }
